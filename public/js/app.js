a31ec03993eeee286bf73bb90a10ad476b489b4d
Felix Stein <felix.stein.vb@gmail.com>
2016-08-12 22:36:54 +0200

    frontend changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,78 @@
+var spotifyItemType = {
+  ARTIST: 1,
+  ALBUM: 2,
+  TRACK: 3,
+  PLAYLIST: 4
+};
+
+var spotifyItemStatus = {
+  QUEUED: 0,
+  DOWNLOADING: 1,
+  FINISHED: 2
+};
+
 new Vue({
   el: '#events',
   data: {
-    event: { name: '', description: '', date: '' },
-    events: []
+    consts: { type: spotifyItemType, status: spotifyItemStatus },
+    spotifyItem: { uri: '', type: '', name: '', artist: '', status: '', date_added: '', date_download_started: '', date_download_finished: '' },
+    queue: []
   },
   ready: function() {
-    this.fetchEvents();
+    this.fetchQueueItems();
   },
   methods: {
-    fetchEvents: function() {
-      var events = [
+    fetchQueueItems: function() {
+      var queue = [
       {
         id: 1,
-        name: 'TIFF',
-        description: 'Toronto International Film Festival',
-        date: '2015-09-10'
-      },
-      {
+        uri: '123',
+        type: spotifyItemType.ALBUM,
+        name: 'abc',
+        artist: 'random guy',
+        status: spotifyItemStatus.QUEUED,
+        date_added: '',
+        date_download_started: '',
+        date_download_finished: ''
+      },{
         id: 2,
-        name: 'The Martian Premiere',
-        description: 'The Martian comes to theatres.',
-        date: '2015-10-02'
-      },
-      {
+        uri: '123',
+        type: spotifyItemType.ALBUM,
+        name: 'def',
+        artist: 'elephant',
+        status: spotifyItemStatus.DOWNLOADING,
+        date_added: '',
+        date_download_started: '',
+        date_download_finished: ''
+      },{
         id: 3,
-        name: 'SXSW',
-        description: 'Music, film and interactive festival in Austin, TX.',
-        date: '2016-03-11'
-      }
+        uri: '123',
+        type: spotifyItemType.ALBUM,
+        name: '123',
+        artist: 'The Artist',
+        status: spotifyItemStatus.FINISHED,
+        date_added: '',
+        date_download_started: '',
+        date_download_finished: ''
+      },
     ];
     // $set is a convenience method provided by Vue that is similar to pushing
     // data onto an array
-    this.$set('events', events);
+    this.$set('queue', queue);
     },
 
     addEvent: function() {
-      if(this.event.name) {
-        this.events.push(this.event);
-        this.event = {name:'',description:'',date:''};
-        var html = '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>' + event.name + '</strong> has been added to the queue</div>';
+      if(this.spotifyItem.uri) {
+        this.queue.push(this.spotifyItem);
+        this.spotifyItem = { uri: '', type: '', name: '', artist: '', status: '', date_added: '', date_download_started: '', date_download_finished: '' };
+        var html = '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>' + spotifyItem.name + '</strong> has been added to the queue</div>';
         $('#alertsection').html(html);
       }
     },
 
     deleteEvent: function(index) {
       if(confirm("you sure to remove " + index)) {
-        this.events.splice(index,1);
+        this.queue.splice(index,1);
       }
     }
   }
